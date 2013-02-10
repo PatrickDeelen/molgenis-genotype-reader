2d2668d7aa2047b508159b3000eb051339979ee2
Patrick Deelen <patrickdeelen@gmail.com>
2013-02-10 15:40:20 +0100

    Also part of last commint about variants IDs

--- src/main/java/org/molgenis/genotype/variant/AbstractGeneticVariant.java
+++ src/main/java/org/molgenis/genotype/variant/AbstractGeneticVariant.java
@@ -1,13 +1,12 @@
 package org.molgenis.genotype.variant;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractGeneticVariant implements GeneticVariant
 {
-	private final List<String> ids;
+	private final GeneticVariantId variantId;
 	private final int startPos;
 	private final String sequenceName;
 	private final Map<String, ?> annotationValues;
@@ -18,13 +17,21 @@ public abstract class AbstractGeneticVariant implements GeneticVariant
 	public AbstractGeneticVariant(List<String> ids, String sequenceName, int startPos, Map<String, ?> annotationValues,
 			Integer stopPos, List<String> altDescriptions, List<String> altTypes)
 	{
-		if (ids == null) throw new IllegalArgumentException("Id list is null");
+		
 		if (sequenceName == null) throw new IllegalArgumentException("SequenceName is null");
 		if (annotationValues == null) throw new IllegalArgumentException("AnnotationValues is null");
 		if (altDescriptions == null) throw new IllegalArgumentException("AltDescriptions is null");
 		if (altTypes == null) throw new IllegalArgumentException("AltTypes is null");
 
-		this.ids = ids;
+		if(ids == null){
+			this.variantId = null;
+		} else if(ids.size() == 1){
+			this.variantId = new SingleGeneticVariantId(ids.get(0));
+		} else {
+			this.variantId = new ListGeneticVariantId(ids);
+		}
+		
+		
 		this.startPos = startPos;
 		this.sequenceName = sequenceName;
 		this.annotationValues = annotationValues;
@@ -33,6 +40,30 @@ public abstract class AbstractGeneticVariant implements GeneticVariant
 		this.altTypes = altTypes;
 	}
 
+	public AbstractGeneticVariant(String id, String sequenceName, int startPos, Map<String, ?> annotationValues,
+			Integer stopPos, List<String> altDescriptions, List<String> altTypes)
+	{
+		
+		if (sequenceName == null) throw new IllegalArgumentException("SequenceName is null");
+		if (annotationValues == null) throw new IllegalArgumentException("AnnotationValues is null");
+		if (altDescriptions == null) throw new IllegalArgumentException("AltDescriptions is null");
+		if (altTypes == null) throw new IllegalArgumentException("AltTypes is null");
+
+		if(id == null){
+			this.variantId = null;
+		} else {
+			this.variantId = new SingleGeneticVariantId(id);
+		}
+		
+		this.startPos = startPos;
+		this.sequenceName = sequenceName;
+		this.annotationValues = annotationValues;
+		this.stopPos = stopPos;
+		this.altDescriptions = altDescriptions;
+		this.altTypes = altTypes;
+	}
+
+	
 	@Override
 	public abstract List<String> getAlleles();
 
@@ -42,28 +73,28 @@ public abstract class AbstractGeneticVariant implements GeneticVariant
 	@Override
 	public String getPrimaryVariantId()
 	{
-		if (ids.isEmpty()) return null;
-		return ids.get(0);
+		if(variantId == null){
+			return null;
+		} else {
+			return variantId.getPrimairyId();
+		}
 	}
 
 	@Override
 	public List<String> getAlternativeVariantIds()
 	{
-		List<String> alternativeIds = new ArrayList<String>();
-
-		String primary = getPrimaryVariantId();
-		if (primary != null)
-		{
-			alternativeIds.addAll(ids);
-			alternativeIds.remove(primary);
-		}
-
-		return Collections.unmodifiableList(alternativeIds);
+		return variantId.getAlternativeIds();
 	}
 
-	public List<String> getIds()
+	@Override
+	public List<String> getAllIds()
 	{
-		return Collections.unmodifiableList(ids);
+		return variantId.getVariantIds();
+	}
+	
+	@Override
+	public GeneticVariantId getVariantId(){
+		return variantId;
 	}
 
 	@Override
