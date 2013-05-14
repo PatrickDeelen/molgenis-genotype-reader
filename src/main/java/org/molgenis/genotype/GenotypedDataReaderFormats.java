ea4fdeaedd73dd164804b0a1a2c9aec9f8654ddb
Patrick Deelen <patrickdeelen@gmail.com>
2013-05-14 21:38:56 +0200

    GenotypeFileTypes + autoloader

---
+++ src/main/java/org/molgenis/genotype/GenotypedDataReaderFormats.java
@@ -0,0 +1,54 @@
+package org.molgenis.genotype;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.molgenis.genotype.multipart.IncompetibleMultiPartGenotypeDataException;
+import org.molgenis.genotype.multipart.MultiPartGenotypeData;
+import org.molgenis.genotype.plink.PedMapGenotypeData;
+import org.molgenis.genotype.vcf.VcfGenotypeData;
+
+public enum GenotypedDataReaderFormats
+{
+
+	PED_MAP("PED / MAP file", "plink PED MAP files gziped with tabix index."), VCF("VCF file",
+			"gziped vcf with tabix index file"), VCF_FOLDER("VCF folder",
+			"Matches all gziped vcf files + tabix index in a folder");
+
+	private final String name;
+	private final String description;
+
+	GenotypedDataReaderFormats(String name, String description)
+	{
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public GenotypeData createGenotypeData(String path, int cacheSize) throws IOException,
+			IncompetibleMultiPartGenotypeDataException
+	{
+
+		switch (this)
+		{
+			case PED_MAP:
+				return new PedMapGenotypeData(new File(path + "map.gz"), new File(path + "map.gz.tbi"), new File(path
+						+ "ped"));
+			case VCF:
+				return new VcfGenotypeData(new File(path), cacheSize);
+			case VCF_FOLDER:
+				return MultiPartGenotypeData.createFromVcfFolder(new File(path), cacheSize);
+			default:
+				throw new RuntimeException("This should not be reachable. Please contact the autors");
+		}
+	}
+}
