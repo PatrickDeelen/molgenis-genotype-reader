e07020025abca630d1c4a9902fcb2ba11695e0f0
Patrick Deelen <patrickdeelen@gmail.com>
2013-05-30 16:48:18 +0200

    Work in progress modifiable genotype data

---
+++ src/main/java/org/molgenis/genotype/modifiable/ModifiableGeneticVariant.java
@@ -0,0 +1,194 @@
+package org.molgenis.genotype.modifiable;
+
+import java.util.List;
+import java.util.Map;
+
+import org.molgenis.genotype.Allele;
+import org.molgenis.genotype.Alleles;
+import org.molgenis.genotype.util.Ld;
+import org.molgenis.genotype.util.LdCalculator;
+import org.molgenis.genotype.util.LdCalculatorException;
+import org.molgenis.genotype.util.MafResult;
+import org.molgenis.genotype.variant.GeneticVariant;
+import org.molgenis.genotype.variant.MafCalculator;
+import org.molgenis.genotype.variant.SampleVariantsProvider;
+import org.molgenis.genotype.variant.id.GeneticVariantId;
+
+public class ModifiableGeneticVariant implements GeneticVariant
+{
+
+	private final GeneticVariant originalVariant;
+	private final ModifiableGenotypeData modifiableGenotypeData;
+	private MafResult mafResult = null;
+
+	public ModifiableGeneticVariant(GeneticVariant originalVariant, ModifiableGenotypeData modifiableGenotypeData)
+	{
+		super();
+		this.originalVariant = originalVariant;
+		this.modifiableGenotypeData = modifiableGenotypeData;
+	}
+
+	@Override
+	public String getPrimaryVariantId()
+	{
+		return getVariantId().getPrimairyId();
+	}
+
+	@Override
+	public List<String> getAlternativeVariantIds()
+	{
+		return getVariantId().getAlternativeIds();
+	}
+
+	@Override
+	public List<String> getAllIds()
+	{
+		return getVariantId().getVariantIds();
+	}
+
+	@Override
+	public GeneticVariantId getVariantId()
+	{
+		GeneticVariantId variantId = modifiableGenotypeData.getUpdatedId(originalVariant);
+		if (variantId != null)
+		{
+			return variantId;
+		}
+		else
+		{
+			return originalVariant.getVariantId();
+		}
+	}
+
+	@Override
+	public int getStartPos()
+	{
+		return originalVariant.getStartPos();
+	}
+
+	@Override
+	public String getSequenceName()
+	{
+		return originalVariant.getSequenceName();
+	}
+
+	@Override
+	public Alleles getVariantAlleles()
+	{
+		Alleles alleles = modifiableGenotypeData.getUpdatedAlleles(originalVariant);
+		if (alleles != null)
+		{
+			return alleles;
+		}
+		else
+		{
+			return originalVariant.getVariantAlleles();
+		}
+	}
+
+	@Override
+	public int getAlleleCount()
+	{
+		return getVariantAlleles().getAlleleCount();
+	}
+
+	@Override
+	public Allele getRefAllele()
+	{
+		Allele refAllele = modifiableGenotypeData.getUpdatedRef(originalVariant);
+		if (refAllele != null)
+		{
+			return refAllele;
+		}
+		else
+		{
+			return originalVariant.getRefAllele();
+		}
+	}
+
+	@Override
+	public List<Alleles> getSampleVariants()
+	{
+		return getSampleVariantsProvider().getSampleVariants(originalVariant);
+	}
+
+	@Override
+	public Map<String, ?> getAnnotationValues()
+	{
+		return originalVariant.getAnnotationValues();
+	}
+
+	@Override
+	public double getMinorAlleleFrequency()
+	{
+		if (mafResult == null)
+		{
+			mafResult = MafCalculator.calculateMaf(getVariantAlleles(), getRefAllele(), getSampleVariants());
+		}
+		return mafResult.getFreq();
+	}
+
+	@Override
+	public Allele getMinorAllele()
+	{
+		if (mafResult == null)
+		{
+			mafResult = MafCalculator.calculateMaf(getVariantAlleles(), getRefAllele(), getSampleVariants());
+		}
+		return mafResult.getMinorAllele();
+	}
+
+	@Override
+	public boolean isSnp()
+	{
+		return getVariantAlleles().isSnp();
+	}
+
+	@Override
+	public boolean isAtOrGcSnp()
+	{
+		return getVariantAlleles().isAtOrGcSnp();
+	}
+
+	@Override
+	public Ld calculateLd(GeneticVariant other) throws LdCalculatorException
+	{
+		return LdCalculator.calculateLd(this, other);
+	}
+
+	@Override
+	public boolean isBiallelic()
+	{
+		return getVariantAlleles().getAlleleCount() == 2;
+	}
+
+	@Override
+	public float[] getSampleDosages()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] getSampleCalledDosage()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SampleVariantsProvider getSampleVariantsProvider()
+	{
+		SampleVariantsProvider sampleVariantProvider = modifiableGenotypeData
+				.getUpdatedSampleVariantProvider(originalVariant);
+		if (sampleVariantProvider != null)
+		{
+			return sampleVariantProvider;
+		}
+		else
+		{
+			return originalVariant.getSampleVariantsProvider();
+		}
+	}
+
+}
--- src/main/java/org/molgenis/genotype/modifiable/ModifiableGenotypeDataInMemory.java
+++ src/main/java/org/molgenis/genotype/modifiable/ModifiableGenotypeDataInMemory.java
@@ -1,168 +1,178 @@
-//package org.molgenis.genotype.modifiable;
-//
-//import java.util.HashMap;
-//import java.util.Iterator;
-//import java.util.List;
-//
-//import org.molgenis.genotype.Alleles;
-//import org.molgenis.genotype.GenotypeData;
-//import org.molgenis.genotype.Sample;
-//import org.molgenis.genotype.Sequence;
-//import org.molgenis.genotype.annotation.Annotation;
-//import org.molgenis.genotype.variant.GeneticVariant;
-//import org.molgenis.genotype.variant.GeneticVariantOld;
-//import org.molgenis.genotype.variant.IllegalReferenceAlleleException;
-//import org.molgenis.genotype.variant.SampleVariantsProvider;
-//import org.molgenis.genotype.variant.SnpGeneticVariant;
-//import org.molgenis.genotype.variant.SwappingSampleVariantsProvider;
-//import org.molgenis.genotype.variant.id.GeneticVariantId;
-//
-//public class ModifiableGenotypeDataInMemory implements ModifiableGenotypeData
-//{
-//
-//	private final GenotypeData sourceGenotypeData;
-//	private final HashMap<GeneticVariantOld, GeneticVariantId> idUpdates;
-//	private final HashMap<GeneticVariantOld, Alleles> refAlleleUpdate;
-//	private final HashMap<GeneticVariantOld, SampleVariantsProvider> variantProviderUpdates;
-//
-//	private final SwappingSampleVariantsProvider swappingSampleVariantProvider;
-//
-//	public ModifiableGenotypeDataInMemory(GenotypeData sourceGenotypeData)
-//	{
-//		super();
-//		this.sourceGenotypeData = sourceGenotypeData;
-//		this.swappingSampleVariantProvider = null; // TODO use
-//													// sourceGenotypeData.getSampleVariantProvider
-//		this.idUpdates = new HashMap<GeneticVariantOld, GeneticVariantId>();
-//		this.refAlleleUpdate = new HashMap<GeneticVariantOld, Alleles>();
-//		this.variantProviderUpdates = new HashMap<GeneticVariantOld, SampleVariantsProvider>();
-//	}
-//
-//	@Override
-//	public List<String> getSeqNames()
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public Iterable<Sequence> getSequences()
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public Sequence getSequenceByName(String name)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public List<GeneticVariant> getVariantsByPos(String seqName, int startPos)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public SnpGeneticVariant getSnpVariantByPos(String seqName, int startPos)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public Iterator<GeneticVariant> getSequenceGeneticVariants(String seqName)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public List<Annotation> getVariantAnnotations()
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public Annotation getVariantAnnotation(String annotationId)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public List<Sample> getSamples()
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public Iterator<GeneticVariant> iterator()
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public GeneticVariantId getUpdatedId(GeneticVariantOld geneticVariant)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public Alleles getUpdatedRef(GeneticVariantOld geneticVariant)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public SampleVariantsProvider getUpdatedSampleVariantProvider(GeneticVariantOld geneticVariant)
-//	{
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public void updateVariantId(GeneticVariantOld geneticVariant, GeneticVariantId newGeneticVariantId)
-//	{
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void updateVariantId(GeneticVariantOld geneticVariant, String newPrimairyId)
-//	{
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void swapGeneticVariant(GeneticVariantOld geneticVariant)
-//	{
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void updateSamplVariantsProvider(GeneticVariantOld geneticVariant,
-//			SampleVariantsProvider newSampleVariantProvider)
-//	{
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//	@Override
-//	public void updateRefAllele(Alleles variantAlleles, Alleles newRefAllele) throws IllegalReferenceAlleleException
-//	{
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-// }
+package org.molgenis.genotype.modifiable;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import org.molgenis.genotype.Allele;
+import org.molgenis.genotype.Alleles;
+import org.molgenis.genotype.RandomAccessGenotypeData;
+import org.molgenis.genotype.Sample;
+import org.molgenis.genotype.Sequence;
+import org.molgenis.genotype.annotation.Annotation;
+import org.molgenis.genotype.variant.GeneticVariant;
+import org.molgenis.genotype.variant.SampleVariantsProvider;
+import org.molgenis.genotype.variant.SwappingSampleVariantsProvider;
+import org.molgenis.genotype.variant.id.GeneticVariantId;
+
+public class ModifiableGenotypeDataInMemory implements ModifiableGenotypeData
+{
+
+	private final RandomAccessGenotypeData sourceGenotypeData;
+	private final HashMap<GeneticVariant, GeneticVariantId> idUpdates;
+	private final HashMap<GeneticVariant, Allele> refAlleleUpdate;
+	private final HashMap<GeneticVariant, Alleles> AllelesUpdate;
+	private final HashMap<GeneticVariant, SampleVariantsProvider> variantProviderUpdates;
+
+	private final HashMap<SampleVariantsProvider, SwappingSampleVariantsProvider> swappingSampleVariantProviders;
+
+	public ModifiableGenotypeDataInMemory(RandomAccessGenotypeData sourceGenotypeData)
+	{
+		super();
+		this.sourceGenotypeData = sourceGenotypeData;
+		this.idUpdates = new HashMap<GeneticVariant, GeneticVariantId>();
+		this.refAlleleUpdate = new HashMap<GeneticVariant, Allele>();
+		this.AllelesUpdate = new HashMap<GeneticVariant, Alleles>();
+		this.variantProviderUpdates = new HashMap<GeneticVariant, SampleVariantsProvider>();
+		this.swappingSampleVariantProviders = new HashMap<SampleVariantsProvider, SwappingSampleVariantsProvider>();
+	}
+
+	@Override
+	public List<String> getSeqNames()
+	{
+		return sourceGenotypeData.getSeqNames();
+	}
+
+	@Override
+	public Iterable<Sequence> getSequences()
+	{
+		return sourceGenotypeData.getSequences();
+	}
+
+	@Override
+	public Sequence getSequenceByName(String name)
+	{
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public List<GeneticVariant> getVariantsByPos(String seqName, int startPos)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GeneticVariant getSnpVariantByPos(String seqName, int startPos)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<GeneticVariant> getSequenceGeneticVariants(String seqName)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Annotation> getVariantAnnotations()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Annotation getVariantAnnotation(String annotationId)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Sample> getSamples()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<GeneticVariant> iterator()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public GeneticVariantId getUpdatedId(GeneticVariant geneticVariant)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Allele getUpdatedRef(GeneticVariant geneticVariant)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SampleVariantsProvider getUpdatedSampleVariantProvider(GeneticVariant geneticVariant)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateVariantId(GeneticVariant geneticVariant, GeneticVariantId newGeneticVariantId)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateVariantId(GeneticVariant geneticVariant, String newPrimairyId)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void swapGeneticVariant(GeneticVariant geneticVariant)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateSamplVariantsProvider(GeneticVariant geneticVariant,
+			SampleVariantsProvider newSampleVariantProvider)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateRefAllele(GeneticVariant geneticVariant, Allele newRefAllele)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateAlleles(GeneticVariant geneticVariant, Alleles newAlleles)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Alleles getUpdatedAlleles(GeneticVariant geneticVariant)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
