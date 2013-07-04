95003a3cb523f15e7d699be5e7cf423b94cb2d3c
Patrick Deelen <patrickdeelen@gmail.com>
2013-07-04 11:25:49 +0200

    extent reader formats with shapeit2

--- src/main/java/org/molgenis/genotype/RandomAccessGenotypedDataReaderFormats.java
+++ src/main/java/org/molgenis/genotype/RandomAccessGenotypedDataReaderFormats.java
@@ -3,6 +3,7 @@ package org.molgenis.genotype;
 import java.io.File;
 import java.io.IOException;
 
+import org.molgenis.genotype.impute2.Impute2GenotypeData;
 import org.molgenis.genotype.multipart.IncompetibleMultiPartGenotypeDataException;
 import org.molgenis.genotype.multipart.MultiPartGenotypeData;
 import org.molgenis.genotype.plink.PedMapGenotypeData;
@@ -13,7 +14,8 @@ public enum RandomAccessGenotypedDataReaderFormats
 
 	PED_MAP("PED / MAP file", "plink PED MAP files gziped with tabix index."), VCF("VCF file",
 			"gziped vcf with tabix index file"), VCF_FOLDER("VCF folder",
-			"Matches all gziped vcf files + tabix index in a folder");
+			"Matches all gziped vcf files + tabix index in a folder"), SHAPEIT2("Shapeit2 output",
+			".haps and .samples with phased haplotypes as outputted by Shapeit2");
 
 	private final String name;
 	private final String description;
@@ -46,6 +48,8 @@ public enum RandomAccessGenotypedDataReaderFormats
 				return new VcfGenotypeData(new File(path), cacheSize);
 			case VCF_FOLDER:
 				return MultiPartGenotypeData.createFromVcfFolder(new File(path), cacheSize);
+			case SHAPEIT2:
+				return new Impute2GenotypeData(bzipHapsFile, tabixIndexFile, sampleFile)
 			default:
 				throw new RuntimeException("This should not be reachable. Please contact the autors");
 		}
